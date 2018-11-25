8697fe168d2aae96fd79e6ef18319334bf053144
Michael Buch_CB <michael.buch@commerzbank.com>
2018-11-25 16:20:56 +0000

    initial creation of the lib components.

---
+++ FBDBLib/view/FBDBLibInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FBDBLib.view
+{
+    class FBDBLibInterface
+    {
+        public int init(string sData)
+        {
+            if (sData.Equals("ok")) return 0;
+            else return -1;
+
+
+        }
+    }
+}
---
+++ FBDBLibUnit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FBDBLibUnit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FBDBLibUnit")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c1ebaa06-98e1-409f-a494-a82d440fb784")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
