bcd26151e318eb022496db655c58cd1737e3e7cc
makshop99 <mbu2015@icloud.com>
2018-11-26 15:45:13 +0100

    added comment for testing azure Devons ci build

--- FBDBLib/view/FBDBLibInterface.cs
+++ FBDBLib/view/FBDBLibInterface.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// added a comment for testing azure devops ci build.
 namespace FBDBLib.view
 {
     public class FBDBLibInterface
